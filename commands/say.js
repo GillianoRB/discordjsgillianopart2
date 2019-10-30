8a86da08a9770277611aa1767fb55cbb94e174c6
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-10-30 18:10:27 -0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 module.exports.run = async(bot, message, args) => {
 
     message.delete();
-     if(!message.member.hasPermission("ADMINISTRATION")) return message.channel.send("No can do pal!") return;
+     if(!message.member.hasPermission("ADMINISTRATION")) return message.channel.send("No can do pal!");
     let botmessage = args.join(" ");
     message.channel.send(botmessage);
   }
