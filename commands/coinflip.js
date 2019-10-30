e384806783719ebdcda1a8ceecfdfc43653004d2
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-10-30 17:35:57 -0300

    Delete coinflip.js

--- commands/coinflip.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-  message.channel.send(`Result: **${Math.floor(Math.random() * 2) == 0 ? "Heads" : "Tails"}**!`);
-}
-
-module.exports.help = {
-  name: "coinflip"
-}
