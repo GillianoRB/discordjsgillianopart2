cd1e3847ea13ead0c2ef318180b7f54218b6dcb2
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-10-30 17:40:06 -0300

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message) => {
-    message.channel.send('Pinging...').then(sent => {
-        sent.edit(`:heartbeat: ${sent.createdTimestamp - message.createdTimestamp}ms`);
-    })
-  .catch(e => logger.error(e));
-};
-module.exports.help = {
-    name: 'ping'
-};
