fb4afaf4ff28a81ceccc3c55d2c1b6bd31738f38
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-11-08 19:18:48 -0300

    Update devsay.js

--- commands/devsay.js
+++ commands/devsay.js
@@ -1,7 +1,7 @@
 module.exports.run = async(bot, message, args) => {
 
     message.delete();
-     if (!message.author.id === '<536702810759233557>') return message.channel.send("No can do pal!");
+     if (!message.author.id === "536702810759233557") return message.channel.send("No can do pal!");
     let botmessage = args.join(" ");
     message.channel.send(botmessage);
   }
